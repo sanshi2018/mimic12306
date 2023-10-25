9b6e1238e4d6d557628dc1c630e092820999bccd
sanshi <49914390+sanshi2018@users.noreply.github.com>
2023-10-25 22:29:24 +0800

    init

---
+++ train-member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -0,0 +1,16 @@
+package com.jiawa.train.member.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+// 因为启动类在config包内，如果未做任何配置，则只能扫描到其所在的包内。
+// 所以需要添加componentScan扫描其他目录
+@ComponentScan("com.jiawa")
+public class MemberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+
+}
