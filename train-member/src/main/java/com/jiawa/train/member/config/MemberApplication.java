7c935083f85f8fdecf60a9f98e6f8901d939be5d
sanshi <49914390+sanshi2018@users.noreply.github.com>
2023-11-02 23:29:56 +0800

    4-7 aspect

--- train-member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ train-member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -1,9 +1,11 @@
 package com.jiawa.train.member.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@Slf4j
 @SpringBootApplication
 // 因为启动类在config包内，如果未做任何配置，则只能扫描到其所在的包内。
 // 所以需要添加componentScan扫描其他目录
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class MemberApplication {
     public static void main(String[] args) {
         SpringApplication.run(MemberApplication.class, args);
+
     }
 
 }
