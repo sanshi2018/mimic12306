1aa1528a5be8607eaa3aa46f2e74f7972b429e1b
sanshi <49914390+sanshi2018@users.noreply.github.com>
2023-11-05 11:55:44 +0800

    4-9 add gateWay

---
+++ gateWay/src/main/java/com/jiawa/train/gateway/config/GateWayApplication.java
@@ -0,0 +1,19 @@
+package com.jiawa.train.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@Slf4j
+@SpringBootApplication
+// 因为启动类在config包内，如果未做任何配置，则只能扫描到其所在的包内。
+// 所以需要添加componentScan扫描其他目录
+@ComponentScan("com.jiawa")
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+
+    }
+
+}
