6906076869808f64a5358c50900938d7bc5882ee
sanshi <49914390+sanshi2018@users.noreply.github.com>
2023-12-18 23:14:47 +0800

    8.4 集成dom4j，读取当前持久层的xml文件，得到表名和实体名

--- gen/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
+++ gen/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
@@ -1,7 +1,6 @@
 package com.jiawa.train.generator.server;
 
 
-import com.jiawa.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -11,7 +10,6 @@ import org.dom4j.io.SAXReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 
 public class ServerGenerator {
     static String toPath = "gen/src/main/java/com/jiawa/train/generator/test/";
@@ -27,6 +25,17 @@ public class ServerGenerator {
 //        Map<String, Object> param = new HashMap<>();
 //        param.put("domain", "Test1");
 //        FreemarkerUtil.generator(toPath + "Test1.java", param);
+        Node node = getGeneratorPath();
+        Document document = new SAXReader().read("gen/" + node.getText());
+        Node table = document.selectSingleNode("//table");
+        System.out.println(table);
+        Node tableNode = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableNode.getText()+"/"+ domainObjectName.getText());
+
+    }
+
+    private static Node getGeneratorPath() throws DocumentException {
         SAXReader saxReader = new SAXReader();
         HashMap<String, String> map = new HashMap<>();
         map.put("pom", "http://maven.apache.org/POM/4.0.0");
@@ -34,5 +43,6 @@ public class ServerGenerator {
         Document document = saxReader.read(pomPath);
         Node node = document.selectSingleNode("//pom:configurationFile");
         System.out.println(node.getText());
+        return node;
     }
 }
