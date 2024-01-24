f4b595829aeeceffe4fd9627decf4abf98bfdaef
sanshi <49914390+sanshi2018@users.noreply.github.com>
2024-01-24 23:24:29 +0800

    11.8 集成openfeign，实现服务间调用。给每个应用增加应用名，可搭配配置中心使用

--- batch/src/main/java/com/jiawa/train/batch/config/BatchApplication.java
+++ batch/src/main/java/com/jiawa/train/batch/config/BatchApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
 
@@ -11,6 +12,7 @@ import org.springframework.core.env.ConfigurableEnvironment;
 @SpringBootApplication
 @ComponentScan("com.jiawa")
 @MapperScan("com.jiawa.train.*.mapper")
+@EnableFeignClients("com.jiawa.train.batch.feign")
 public class BatchApplication {
 
     public static void main(String[] args) {
--- batch/src/main/java/com/jiawa/train/batch/controller/TestController.java
+++ batch/src/main/java/com/jiawa/train/batch/controller/TestController.java
@@ -1,12 +1,22 @@
 package com.jiawa.train.batch.controller;
 
+import com.jiawa.train.batch.feign.BusinessFeign;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
+
+    @Resource
+    BusinessFeign businessFeign;
+
     @GetMapping("/hello")
     public String hello() {
-        return "hello!!!";
+        String hello = businessFeign.hello();
+        log.info("feigh res: {}", hello);
+        return "Hello ! Batch";
     }
 }
