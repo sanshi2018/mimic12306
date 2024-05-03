39f9769caaf33dc7a2a49bc62de94170e4eb44e9
sanshi <49914390+sanshi2018@users.noreply.github.com>
2024-05-03 19:24:04 +0800

    14.4 使用SpringBoot内置的缓存

--- business/src/main/java/com/jiawa/train/business/req/DailyTrainTicketQueryReq.java
+++ business/src/main/java/com/jiawa/train/business/req/DailyTrainTicketQueryReq.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -31,6 +32,23 @@ public class DailyTrainTicketQueryReq extends PageReq {
      * 到达站
      */
     private String end;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DailyTrainTicketQueryReq that)) return false;
+        return Objects.equals(date, that.date)
+                && Objects.equals(trainCode, that.trainCode)
+                && Objects.equals(start, that.start)
+                && Objects.equals(end, that.end)
+                && Objects.equals(((DailyTrainTicketQueryReq) o).getPage(), that.getPage())
+                && Objects.equals(((DailyTrainTicketQueryReq) o).getSize(), that.getSize());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, trainCode, start, end, getPage(), getSize());
+    }
 
     @Override
     public String toString() {
