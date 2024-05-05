bd8da025aa3628728fa332d698fbad29baee2b6d
sanshi <49914390+sanshi2018@users.noreply.github.com>
2024-05-05 11:23:20 +0800

    17.6 代码中不写死限流规则，通过控台来配置，但是控台配置后，重启会清空所有的配置。接口的资源名称不要和接口路径一致，会导致限流后走不到降级方法中

--- business/src/main/java/com/jiawa/train/business/controller/ConfirmOrderController.java
+++ business/src/main/java/com/jiawa/train/business/controller/ConfirmOrderController.java
@@ -1,15 +1,20 @@
 package com.jiawa.train.business.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.jiawa.train.business.req.ConfirmOrderDoReq;
 import com.jiawa.train.business.service.ConfirmOrderService;
+import com.jiawa.train.common.exception.BusinessExceptionEnum;
 import com.jiawa.train.common.resp.CommonResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/confirm-order")
 public class ConfirmOrderController {
@@ -17,10 +22,26 @@ public class ConfirmOrderController {
     @Resource
     private ConfirmOrderService confirmOrderService;
 
+    // 接口的资源名称不要和接口路径一致，会导致限流后走不到降级方法中
+    @SentinelResource(value = "confirmOrderDo", blockHandler = "doConfirmBlock")
     @PostMapping("/do")
     public CommonResp<Object> doConfirm(@Valid @RequestBody ConfirmOrderDoReq req) {
         confirmOrderService.doConfirm(req);
         return new CommonResp<>();
+    }
+
+    /** 降级方法，需包含限流方法的所有参数和BlockException参数，且返回值要保持一致
+     * @param req
+     * @param e
+     */
+    public CommonResp<Object> doConfirmBlock(ConfirmOrderDoReq req, BlockException e) {
+        log.info("ConfirmOrderController购票请求被限流：{}", req);
+        // throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION);
+        CommonResp<Object> commonResp = new CommonResp<>();
+        commonResp.setSuccess(false);
+        commonResp.setMessage(BusinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION.getDesc());
+        return commonResp;
+
     }
 
 }
