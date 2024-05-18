2bfddbf310a6bc99c8d84c117a7255a52a9589a1
sanshi <49914390+sanshi2018@users.noreply.github.com>
2024-05-18 23:55:06 +0800

    19.10 MQ消费里，没抢到锁的，表示有其它消费线程正在出票，不做任何处理

--- business/src/main/java/com/jiawa/train/business/service/ConfirmOrderService.java
+++ business/src/main/java/com/jiawa/train/business/service/ConfirmOrderService.java
@@ -131,8 +131,10 @@ public class ConfirmOrderService {
 
             } else {
                 // 只是没抢到锁，并不知道票抢完了没，所以提示稍候再试
-                LOG.info("很遗憾，没抢到锁");
-                throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_LOCK_FAIL);
+//                LOG.info("很遗憾，没抢到锁");
+//                throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_LOCK_FAIL);
+                LOG.info("没抢到锁，有其它消费线程正在出票，不做任何处理");
+                return;
             }
 
             while (true) {
