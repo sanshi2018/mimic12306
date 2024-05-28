032023965141be90ac562f44aed4d35e3916e1b6
sanshi <49914390+sanshi2018@users.noreply.github.com>
2024-05-28 20:46:01 +0800

    feign 配置优先级设置

--- common/src/main/java/com/jiawa/train/common/interceptor/OkHttpReqInterceptor.java
+++ common/src/main/java/com/jiawa/train/common/interceptor/OkHttpReqInterceptor.java
@@ -4,11 +4,16 @@ import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Slf4j
+// 如何需要spring的bean注入使用，那么这个类需要交给spring管理
+// 但是如果交给spring管理，那么这个类会作为全局配置生效
+// 所以需要设置 client.defaultToProperties = false; (默认为true)
+//@Component
 public class OkHttpReqInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate requestTemplate) {
